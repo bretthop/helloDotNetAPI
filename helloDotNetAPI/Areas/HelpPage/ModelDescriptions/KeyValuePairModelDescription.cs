810eec14df0a1ad7e2c1e8c616bcbbd2160fe7e1
Brett Hopper <brett.hoppz@gmail.com>
2016-07-05 16:48:37 +1000

    initial commit

---
+++ helloDotNetAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace helloDotNetAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
