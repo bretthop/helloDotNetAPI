e0f76559c79e9e0592a7a2ae8d9884045ac4219b
Brett Hopper <brett.hoppz@gmail.com>
2016-07-06 10:16:45 +1000

    Small change to test Azure deploy

--- helloDotNetAPI/Controllers/ValuesController.cs
+++ helloDotNetAPI/Controllers/ValuesController.cs
@@ -12,13 +12,13 @@ namespace helloDotNetAPI.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1 - changed", "value2 - changed" };
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return "value - changed";
         }
 
         // POST api/values
