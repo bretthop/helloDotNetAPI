02ed9db139d2e57ba083ea9a104007f8220f586e
Brett Hopper <brett.hoppz@gmail.com>
2016-07-13 16:58:48 +1000

    Added unit testing using Microsfot's framework

---
+++ helloDotNetAPI/Models/HelloTestEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace helloDotNetAPI.Models
+{
+    public class HelloTestEntity
+    {
+        public string TestProperty { get; set; }
+
+        public string ReturnTestPropertyForUnitTest()
+        {
+            return TestProperty;
+        }
+    }
+}
---
+++ helloDotNetTests/HelloTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using helloDotNetAPI.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace helloDotNetTests
+{
+    [TestClass]
+    public class HelloTests
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            HelloTestEntity testEntity = new HelloTestEntity();
+            testEntity.TestProperty = "test";
+            string returnedTestProperty = testEntity.ReturnTestPropertyForUnitTest();
+
+            Assert.AreEqual("test", returnedTestProperty);
+        }
+    }
+}
